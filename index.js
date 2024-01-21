b944e85f1b02518c7908c2f1edf8275652c71179
Allan Saenz <allan.d.saenz@gmail.com>
2024-01-21 14:45:54 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(array, target){
+  //For every index/number in the array...
+  for (let i = 0; i <= array.length; i++){
+  //...we'll iterate over every other index/number in the array...
+    for(let j = i+1; j <= array.length; j++){
+  //...for each index of i and j compared we'll check if when added to each other they equal the target sum...
+      if (array[i] + array[j] === target){
+  //...if they do return true...
+        return true
+      }
+    }
+  //...if they do not, return false. 
+  } return false
 }
-
 /* 
   Write the Big O time complexity of your function here
+  O(n²)
 */
 
 /* 
   Add your pseudocode here
+
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here: 
+
 */
 
 // You can run `node index.js` to view these console logs
